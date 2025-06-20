ab0ac06a5d7b2b6eb2bac1a062e240e0eabf5541
nikolai-vasilyev <vasiljev.nickolo@yandex.ru>
2025-06-20 08:13:29 +0000

    update config

--- terra_cluster/providers.tf
+++ terra_cluster/providers.tf
@@ -22,6 +22,7 @@ terraform {
 provider "yandex" {
   cloud_id  = var.cloud_id
   folder_id = var.folder_id
+  token = var.token
   #  zone                     = var.default_zone
   #  service_account_key_file = file("~/Project_diplom/key.json")
 }
