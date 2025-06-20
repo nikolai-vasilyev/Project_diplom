7af231d2106c87057bbda6b13f89911448677199
nikolai-vasilyev <vasiljev.nickolo@yandex.ru>
2025-06-20 06:00:50 +0000

    workflow files

--- terra_cluster/providers.tf
+++ terra_cluster/providers.tf
@@ -23,6 +23,6 @@ provider "yandex" {
   cloud_id  = var.cloud_id
   folder_id = var.folder_id
   #  zone                     = var.default_zone
-  service_account_key_file = file("~/Project_diplom/key.json")
+  #  service_account_key_file = file("~/Project_diplom/key.json")
 }
 
