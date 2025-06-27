c068ef1c5973ddfe6614c1857b57b0c3c1b51dcd
nikolai-vasilyev <vasiljev.nickolo@yandex.ru>
2025-06-27 13:03:27 +0300

    destroy please

--- terra_cluster/outputs.tf
+++ terra_cluster/outputs.tf
@@ -3,7 +3,7 @@ output "cluster" {
     { control = [for i in yandex_compute_instance.control : "${i.name}: ssh alma@${i.network_interface.0.nat_ip_address} , ssh alma@${i.network_interface.0.ip_address} "] },
     { work-b = [for i in yandex_compute_instance.work-b : "${i.name}: ssh alma@${i.network_interface.0.nat_ip_address} , ssh alma@${i.network_interface.0.ip_address} "] },
     { work-d = [for i in yandex_compute_instance.work-d : "${i.name}: ssh alma@${i.network_interface.0.nat_ip_address} , ssh alma@${i.network_interface.0.ip_address} "] },
-    { NLB-web = yandex_lb_network_load_balancer.lb-cluster-web.listener.*.external_address_spec[0].*.address },
+    { NLB-test-app = yandex_lb_network_load_balancer.lb-cluster-web.listener.*.external_address_spec[0].*.address },
     { NLB-prometheus = yandex_lb_network_load_balancer.lb-cluster-monitoring.listener.*.external_address_spec[0].*.address }
   ]
 }
